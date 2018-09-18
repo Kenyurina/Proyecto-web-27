01cad309826f53c79811bfb86ffed132560947ad
Kenyuri Camino <Kenyuri9@gmail.com>
2018-09-18 11:48:02 -0400

    Proyecto-web-27-globe

---
+++ js/Modal.js
@@ -0,0 +1,190 @@
+// Obtenemos el modal
+var modal = document.getElementById('myModal');
+
+// Obteneos la imagen y la insertamos dentro del modal
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Obtenemos el elemento span que cierra el  modal
+var span = document.getElementsByClassName("close")[0];
+
+// Cuando el usuario haga clic en el span (x) cierra el modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+
+
+// Obtenemos el modal
+var modal = document.getElementById('myModal');
+
+// Obteneos la imagen y la insertamos dentro del modal
+var img = document.getElementById('myImga');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Obtenemos el elemento span que cierra el  modal
+var span = document.getElementsByClassName("close")[0];
+
+// Cuando el usuario haga clic en el span (x) cierra el modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+
+// Obtenemos el modal
+var modal = document.getElementById('myModal');
+
+// Obteneos la imagen y la insertamos dentro del modal
+var img = document.getElementById('myImgb');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Obtenemos el elemento span que cierra el  modal
+var span = document.getElementsByClassName("close")[0];
+
+// Cuando el usuario haga clic en el span (x) cierra el modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+// Obtenemos el modal
+var modal = document.getElementById('myModal');
+
+// Obteneos la imagen y la insertamos dentro del modal
+var img = document.getElementById('myImgc');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Obtenemos el elemento span que cierra el  modal
+var span = document.getElementsByClassName("close")[0];
+
+// Cuando el usuario haga clic en el span (x) cierra el modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+
+// Obtenemos el modal
+var modal = document.getElementById('myModal');
+
+// Obteneos la imagen y la insertamos dentro del modal
+var img = document.getElementById('myImgd');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Obtenemos el elemento span que cierra el  modal
+var span = document.getElementsByClassName("close")[0];
+
+// Cuando el usuario haga clic en el span (x) cierra el modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+
+// Obtenemos el modal
+var modal = document.getElementById('myModal');
+
+// Obteneos la imagen y la insertamos dentro del modal
+var img = document.getElementById('myImge');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Obtenemos el elemento span que cierra el  modal
+var span = document.getElementsByClassName("close")[0];
+
+// Cuando el usuario haga clic en el span (x) cierra el modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+
+// Obtenemos el modal
+var modal = document.getElementById('myModal');
+
+// Obteneos la imagen y la insertamos dentro del modal
+var img = document.getElementById('myImgf');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Obtenemos el elemento span que cierra el  modal
+var span = document.getElementsByClassName("close")[0];
+
+// Cuando el usuario haga clic en el span (x) cierra el modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+
+// Obtenemos el modal
+var modal = document.getElementById('myModal');
+
+// Obteneos la imagen y la insertamos dentro del modal
+var img = document.getElementById('myImgg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Obtenemos el elemento span que cierra el  modal
+var span = document.getElementsByClassName("close")[0];
+
+// Cuando el usuario haga clic en el span (x) cierra el modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
+
+
+
+
+
+
+
---
+++ js/timer.js
@@ -0,0 +1,11 @@
+  $(document).ready(function() {    
+    $("#transition-timer-carousel").on("slide.bs.carousel", function(event) {
+        $(".transition-timer-carousel-progress-bar", this)
+            .removeClass("animate").css("width", "0%");
+    }).on("slid.bs.carousel", function(event) {
+        $(".transition-timer-carousel-progress-bar", this)
+            .addClass("animate").css("width", "100%");
+    });
+    $(".transition-timer-carousel-progress-bar", "#transition-timer-carousel")
+        .css("width", "100%");
+});
